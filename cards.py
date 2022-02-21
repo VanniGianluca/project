344562d2632cb796f02c595e0626536947b1ed0c
vannigianluca <vannigianluca8892@gmail.com>
2022-02-21 20:46:56 +0200

    deck works and can be shuffled, can see first card

--- cards.py
+++ cards.py
@@ -1,33 +1,40 @@
 # some exercises about a deck of cards_thought
 import random
+import itertools
 
+# define the lists needed
 cards_thought = []
 suits = ('spade', 'coppe', 'denari', 'bastoni')
 numbers = (range(1, 11))
 decks = []
+hand = []
+c = 0
 
-class Card:
+
+class Card :
     '''class representing a card of a deck'''
-    def __init__(self, number, suit):
+
+    def __init__(self, number, suit) :
         '''initialise card object'''
         suits = ('spade', 'coppe', 'denari', 'bastoni')
         numbers = list(range(1, 11))
         self.number = number
         self.suit = suit
-        if self.suit not in suits:
+        if self.suit not in suits :
             raise ValueError("Il seme non è valido!")
-        if self.number not in numbers:
+        if self.number not in numbers :
             raise ValueError("Il seme non è valido!")
 
-    def __str__(self):
+    def __str__(self) :
         '''describes better the card'''
         return f'Questa carta è un {self.number} di {self.suit}'
 
-    def describe_card(self):
+    def describe_card(self) :
         '''describe the card'''
         print(f'Questa carta è un {self.number} di {self.suit}')
 
-def think_random_card():
+
+def think_random_card() :
     '''generate a random card and append it to cards_thought list'''
     suits = ('spade', 'coppe', 'denari', 'bastoni')
     numbers = list(range(1, 11))
@@ -35,31 +42,54 @@ def think_random_card():
     cards_thought.append(new_card)
     print(f'Sto pensando a un {new_card.number} di {new_card.suit}.')
 
-class Deck:
+
+# def cycle(iterable) :
+#     # cycle('ABCD') --> A B C D A B C D A B C D ...
+#     saved = []
+#     for element in iterable :
+#         yield element
+#         saved.append(element)
+#     while saved :
+#         for element in saved :
+#             yield element
+
+
+class Deck :
     '''a class representing a deck'''
-    def __init__(self, cards_deck=[]):
+
+    def __init__(self, cards_deck=[]) :
         '''initialise deck attributes'''
         self.cards_deck = cards_deck
 
-    def __str__(self):
+    def __str__(self) :
         '''better describe the deck'''
         return f'Questo mazzo ha {len(self.cards_deck)} carte.'
 
-    def shuffle_deck(self):
+    def shuffle_deck(self) :
+        '''shuffle the deck'''
         print('Il mazzo è stato mischiato.')
         return random.shuffle(self.cards_deck)
 
-    def look_next_card(self):
-        self.cards_deck = iter(self.cards_deck)
-        return print(next(self.cards_deck, 'Il mazzo è finito.'))
+    def first_card(self) :
+        #     '''look at the first card in the deck'''
+        return print(self.cards_deck[0])
 
 
-def create_new_deck():
+
+
+    # def see_cards(self):
+    #     #     '''look at the cards in the deck one after the other'''
+    #     i1 = itertools.cycle(self.cards_deck)
+    #     c = 1
+    #     for a in i1:
+    #         print(a)
+    #         c+=1
+    #         if c==len(self.cards_deck):
+    #             break
+
+
+def create_new_deck() :
     '''create a new dew'''
     new_deck = Deck()
     new_deck.cards_deck = [Card(number, suit) for number in numbers for suit in suits]
     return decks.append(new_deck)
-
-
-
-
--- try.py
+++ try.py
@@ -8,17 +8,37 @@ think_random_card()
 print(f'Ora creerò un mazzo...')
 create_new_deck()
 print(f'{decks[0]}')
-print(f'Ora elencherò le prime 10 carte del mazzo...')
-for a in range(10):
-    decks[0].look_next_card()
-print(f'Ora elencherò le prossime 20 carte del mazzo...')
-for a in range(20):
-    decks[0].look_next_card()
-print(f'Ora elencherò le ultime 10 carte del mazzo...')
-for a in range(10):
-    decks[0].look_next_card()
-decks[0].look_next_card()
 print(f'Ora mischierò il mazzo...')
 decks[0].shuffle_deck()
-print(f'Ora guarderò la prossima carta del mazzo...')
-decks[0].look_next_card()
+print(f'Ora elencherò la prima carta del mazzo...')
+decks[0].first_card()
+print(f'Ora elencherò la seconda carta del mazzo...')
+decks[0].first_card()
+# print(f'Ora elencherò le prossime 9 carte del mazzo...')
+# for a in range(9):
+#     cycle(decks[0])
+# print(f'Ora elencherò le prossime 20 carte del mazzo...')
+# for a in range(20):
+#     decks[0].look_next_card()
+# print(f'Ora elencherò le ultime 10 carte del mazzo...')
+# for a in range(10):
+#     decks[0].look_next_card()
+# decks[0].look_next_card()
+
+# print(f'Ora guarderò la prossima carta del mazzo...')
+#
+# print(f'Ora elencherò le prime 10 carte del mazzo...')
+# for a in range(10):
+#     decks[0].look_next_card()
+# print(f'Ora elencherò le prossime 20 carte del mazzo...')
+# for a in range(20):
+#     decks[0].look_next_card()
+# print(f'Ora elencherò le ultime 10 carte del mazzo...')
+# for a in range(10):
+#     decks[0].look_next_card()
+# decks[0].look_next_card()
+# print(f'Ora mischierò il mazzo...')
+# decks[0].shuffle_deck()
+# print(f'Ora guarderò la prossima carta del mazzo...')
+
+
